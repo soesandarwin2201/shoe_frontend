2a65e6b926c0f53025f091e09ebf3194ad6bfb79
Soe Sandar Win <soesandarwin2201@gmail.com>
2023-05-01 12:09:28 +0630

    get the input value using onchange

--- src/components/singup/singup.jsx
+++ src/components/singup/singup.jsx
@@ -2,14 +2,21 @@ import React, { useState} from "react";
 import { signupUser } from "../../Redux/signup/signupSlice";
 
 const Singup = () => {
+     const [name,setName] = useState('');
+     const [email,setEmail] = useState('');
+     const [password,setPassword] = useState('');
      return (  
           <section className="signup-container">
                <h1 className="signup-title">SignUp</h1>
                <form className="signup-form">
-                    <input type="text" name="name" placeholder="Please Write your name" className="form-input" />
+                    <input type="text" name="name" placeholder="Please Write your name" className="form-input" 
+                    onChange={(e) => setName(e.target.value)} />
                     <input type="email" className="form-input" 
-                    name="email" placeholder="Please write your email" />
-                    <input type="password" className="form-input" name="password" placeholder="Please Enter your password" />
+                    name="email" placeholder="Please write your email" 
+                    onChange={(e) => setEmail(e.target.value)} />
+                    <input type="password" className="form-input" name="password" placeholder="Please Enter your password"  
+                    onChange={(e) => setPassword(e.target
+                         .value)} />
                     <button className="form-button">SignUp</button>
                </form>
                <div className="questions">
